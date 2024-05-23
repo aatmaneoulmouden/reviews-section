8d8f7b0feaad133ca3cf33e651d87b26b2e69f7b
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-05-23 19:39:24 +0100

    Theme configruation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'white': '#f9f7f3',
+      'black': '#051923',
+      'primary': '#0fa3b1',
+      'hover': '#0c828e',
+    },
+    fontFamily: {
+      'poppins': ['Poppins' ,'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
