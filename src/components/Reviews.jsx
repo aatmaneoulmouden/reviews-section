efb79c7f6b02b209cc73b60cbfb49245a3464570
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-05-30 19:27:15 +0100

    Create data file (reviews) and render using the new method

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,45 +1,20 @@
 import SingleReview from "./SingleReview";
+import ReviewsData from "../data.js";
 
 const Reviews = () => {
-  const reviews = [
-    {
-      name: "Aatmane Oulmouden",
-      title: "Frontend developer",
-      comment:
-        "This product has significantly improved our team's workflow efficiency. The user-friendly interface and robust features are exactly what we needed. Highly recommend to anyone in the industry.",
-      numOfStars: 5,
-      imgUrl: "./aatmane.jpg",
-    },
-    {
-      name: "Emily Johnson",
-      title: "UX Designer",
-      comment:
-        "The design and functionality are top-notch. However, the learning curve was steeper than expected. Once you get the hang of it, it's a fantastic tool for design and development collaboration.",
-      numOfStars: 4,
-      imgUrl: "./emily.jpg",
-    },
-    {
-      name: "John Smith",
-      title: "Software Engineer",
-      comment:
-        "A solid tool with some great features. It has some bugs that need fixing, but overall it has been very useful for our projects. Customer support is responsive and helpful.",
-      numOfStars: 3,
-      imgUrl: "./john.jpg",
-    },
-  ];
-
+  const reviews = ReviewsData.map((review, index) => (
+    <SingleReview
+      key={index}
+      name={review.name}
+      title={review.title}
+      comment={review.comment}
+      stars={review.numOfStars}
+      image={review.imgUrl}
+    />
+  ));
   return (
     <div className="reviews-wrapper grid grid-cols-1 gap-y-24 mt-24 lg:gap-x-8 lg:grid-cols-3">
-      {reviews.map((review, index) => (
-        <SingleReview
-          key={index}
-          name={review.name}
-          title={review.title}
-          comment={review.comment}
-          stars={review.numOfStars}
-          image={review.imgUrl}
-        />
-      ))} 
+      {reviews}
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default ([
+    {
+        "name": "Aatmane Oulmouden",
+        "title": "Frontend developer",
+        "comment": "This product has significantly improved our team's workflow efficiency. The user-friendly interface and robust features are exactly what we needed. Highly recommend to anyone in the industry.",
+        "numOfStars": 5,
+        "imgUrl": "./aatmane.jpg"
+    },
+    {
+        "name": "Emily Johnson",
+        "title": "UX Designer",
+        "comment": "The design and functionality are top-notch. However, the learning curve was steeper than expected. Once you get the hang of it, it's a fantastic tool for design and development collaboration.",
+        "numOfStars": 4,
+        "imgUrl": "./emily.jpg"
+    },
+    {
+        "name": "John Smith",
+        "title": "Software Engineer",
+        "comment": "A solid tool with some great features. It has some bugs that need fixing, but overall it has been very useful for our projects. Customer support is responsive and helpful.",
+        "numOfStars": 3,
+        "imgUrl": "./john.jpg"
+    }
+])
